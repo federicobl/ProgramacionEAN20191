dc44f936874e53ba2e9f356d3eb1973e2ba46ccb
FEDERICO <fbustos70450@universidadean.edu.co>
2019-03-05 18:30:00 -0500

    SEMANA 5

---
+++ semana6/ejemplos semana 6.py
@@ -0,0 +1,14 @@
+def es_parentesis(caracter):
+    """
+    
+    (str of len ==1) -> str
+    
+    >>> es_parentesis('(')
+    'Es parentesis'
+    
+    >>> es_parentesis('x')
+    'No Es parentesis'
+    
+    :param caracter: str el caracter a evaluar
+    :return: str el mensaje de la validacion
+    """

3a105d9e342d44e426462b1302fb328271d993db
FEDERICO <fbustos70450@universidadean.edu.co>
2019-03-05 18:43:56 -0500

    SEMANA 5

--- Semana_6/ejemplos semana 6.py
+++ Semana_6/ejemplos semana 6.py
@@ -1,14 +1,20 @@
 def es_parentesis(caracter):
     """
-    
+
     (str of len ==1) -> str
-    
+
     >>> es_parentesis('(')
     'Es parentesis'
-    
+
     >>> es_parentesis('x')
     'No Es parentesis'
-    
+
+    >>> es_parentesis('xa')
+    Tracebackv (most recent call last):
+    ..
+    TypeError: xa no es un parentesis
+
     :param caracter: str el caracter a evaluar
     :return: str el mensaje de la validacion
     """
+
